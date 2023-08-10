ee0433bfe1333d561e21447537cc9bb0dbda26f1
ahmedasad236 <ahmedasad236@gmail.com>
2023-08-10 10:39:07 +0300

    Starter files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="container">
+    <h1 :class="heading">To Do list</h1>
+    <form></form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+  padding: 2rem;
+  gap: 2;
+}
+
+.heading {
+  color: rgb(36, 53, 87);
+  font-weight: 600;
+}
+</style>
