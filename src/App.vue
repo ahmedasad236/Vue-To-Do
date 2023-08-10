0904e362bb6c46faf1f6790ed6e41dd4e834d4a9
ahmedasad236 <ahmedasad236@gmail.com>
2023-08-10 11:14:15 +0300

    Create a simple todo app and finish the logic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
-  <div :class="container">
-    <h1 :class="heading">To Do list</h1>
-    <form></form>
+  <div class="container">
+    <h1 class="heading">Todos</h1>
+    <div class="display">
+      <ul class="todos">
+        <li
+          v-for="todo in todos"
+          :key="todo"
+        >
+          <div>
+            <input type="checkbox" />
+            <sapn>{{ todo }}</sapn>
+            <button @click="removeFromTodos(todo)">&times;</button>
+          </div>
+        </li>
+      </ul>
+      <div v-if="showCurrentTodo">
+        <input
+          v-model="currentTodo"
+          id="enter-todo"
+          placeholder="Enter todo..."
+        />
+        <button @click="addToTodo(currentTodo)">&#x2713;</button>
+      </div>
+      <button @click="showTodo()">+</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      todos: ['hello'],
+      currentTodo: '',
+      showCurrentTodo: false
+    };
+  },
+  methods: {
+    removeFromTodos(todo) {
+      this.todos = this.todos.filter((item) => item != todo);
+    },
+
+    addToTodo(todo) {
+      if (!this.currentTodo) return;
+
+      this.todos.push(todo);
+      this.currentTodo = '';
+      this.showCurrentTodo = false;
+    },
+
+    showTodo() {
+      this.showCurrentTodo = true;
+    }
+  }
 };
 </script>
 
@@ -15,15 +61,20 @@ export default {
 .container {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: #ccc;
-  padding: 2rem;
+  background-color: #15191b;
+  color: aliceblue;
+  padding: 5rem;
   gap: 2;
+  height: 100vh;
+}
+
+.display {
+  display: flex;
+  flex-direction: column;
 }
 
 .heading {
-  color: rgb(36, 53, 87);
+  color: #eae287;
   font-weight: 600;
 }
 </style>
